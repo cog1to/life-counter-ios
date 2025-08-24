62a8d3c320b9073ba85b0cbce667d2458415a163
cog1to <sorryforbadname@gmail.com>
2025-08-24 23:34:06 +0400

    Initial Commit

---
+++ life-counter-ios/life_counter_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  life_counter_iosApp.swift
+//  life-counter-ios
+//
+//  Created by Alexander on 24.08.25.
+//
+
+import SwiftUI
+
+@main
+struct life_counter_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
